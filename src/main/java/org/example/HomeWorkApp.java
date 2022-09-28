de4b33a34ddc4b50d4c9432fe05de0ca8f3d68ae
chip3x <chip3x@mail.ru>
2022-09-28 22:16:44 +0300

    Lesson_1

---
+++ src/main/java/org/example/HomeWorkApp.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords("Orange", "Banana", "Apple");
+        checkSumSign(3,5);
+        printColor(5);
+        compareNumbers(2,6);
+        }
+
+    public static void printThreeWords(String a, String b, String c){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    public static void checkSumSign(int a, int b){
+        int c = a +b;
+        if (c >=0 ){
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(int value ){
+        if (value <= 0){
+            System.out.println("Красный");
+        }  else if (value <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(int a, int b){
+        if (a >= b){
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
